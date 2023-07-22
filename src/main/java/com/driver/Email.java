407c4bdb7f040aa8035460119dc41410f75fc1f9
AKr2704 <adarshkr.1993@gmail.com>
2023-07-22 13:10:16 +0530

    Code updated in Assignment VIII - Emails.

--- src/main/java/com/driver/Email.java
+++ src/main/java/com/driver/Email.java
@@ -25,5 +25,27 @@ public class Email {
         // 3. It contains at least one lowercase letter
         // 4. It contains at least one digit
         // 5. It contains at least one special character. Any character apart from alphabets and digits is a special character
+        if(password.equals(oldPassword)) {
+            if(isValidPass(newPassword)){
+                this.password = newPassword;
+            }
+        }
+    }
+    private boolean isValidPass(String newPass){
+        int len = newPass.length();
+        boolean hasUC = false;  //UC - UpperCase
+        boolean hasLC = false;  //LC - Lowercase
+        boolean hasDig = false; //Dig - Digit
+        boolean hasSC = false;  //SC - Special Character
+        if(len<8) return false;
+        for(int i =0; i<len; i++){
+            char val = newPass.charAt(i);
+            if(val>='A' && val<='Z') hasUC = true;
+            else if(val>='0' && val<='9')  hasDig = true;
+            else if(val>='a' && val<='z')  hasLC = true;
+            else hasSC = true;
+        }
+        if(hasSC && hasLC && hasUC && hasDig) return true;
+        return false;
     }
 }
